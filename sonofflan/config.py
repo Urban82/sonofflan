5cad598104d1a1eb6b39e1e556dcaee54bd1aa3a
Danilo Treffiletti <urban82@gmail.com>
2022-11-21 15:45:13 +0100

    Class DevicesConfig

--- sonofflan/config.py
+++ sonofflan/config.py
@@ -55,3 +55,75 @@ class DeviceConfig:
         """The device encryption key (or None if not set)"""
 
         return self._key
+
+
+class DevicesConfig:
+    """Hold the configuration for all the devices.
+
+    Configuration is loaded by list of dictionaries or by dictionary:
+
+        [
+            {
+                "id": "1234",
+                "name": "Device 1"
+            },
+            {
+                "id": "5678",
+                "name": "Device 2"
+            }
+        ]
+
+    or
+
+        {
+            "1234": {
+                "name": "Device 1"
+            },
+            "5678" : {
+                "name": "Device 2"
+            }
+        }
+    """
+
+    _devices = {}
+
+    def __init__(self, c: dict|list|None = None) -> None:
+        """
+        Parameters
+        ----------
+        `c` : dict|list
+            Dictionary or list with the configuration (optional)
+        """
+
+        if c is None:
+            return # Nothing to configure
+        if isinstance(c, dict):
+            # Configure from dictionary: ID is in the key
+            for id in c:
+                c[id]["id"] = id # In that case field "id" is replaced with the key
+                self._devices[id] = DeviceConfig(c[id])
+        elif isinstance(c, list):
+            # Configure from list: ID is in the item
+            for it in c:
+                dc = DeviceConfig(it)
+                self._devices[dc.id] = dc
+        else:
+            raise TypeError(f"Invalid parameter \"{c}\" ({type(c).__name__}): expected a dictionary or a list")
+
+    def num_devices(self) -> int:
+        """Get the number of configured devices"""
+
+        return len(self._devices)
+
+    def device(self, id: str) -> DeviceConfig|None:
+        """Get the configuration for the device with the specified ID
+
+        Parameters
+        ----------
+        `id` : str
+            The ID for the device to retrieve
+        """
+
+        if id in self._devices:
+            return self._devices[id]
+        return None
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,6 @@
 import pytest
 
-from sonofflan.config import DeviceConfig
+from sonofflan.config import DeviceConfig, DevicesConfig
 
 
 # Tests for DeviceConfig
@@ -29,3 +29,67 @@ def test_deviceconfig_with_key():
     assert dc.id == "1234"
     assert dc.name == "ABCD"
     assert dc.key == "!£$%"
+
+
+# Tests for DevicesConfig
+def test_devicesconfig_invalid():
+    with pytest.raises(TypeError):
+        DevicesConfig("")
+
+def test_devicesconfig_none():
+    dc = DevicesConfig()
+    assert dc.num_devices() == 0
+    assert dc.device("1234") is None
+    assert dc.device("5678") is None
+    assert dc.device("ABCD") is None
+
+def test_devicesconfig_dict():
+    dc = DevicesConfig({
+        "1234": {
+            "name": "Device 1"
+        },
+        "5678" : {
+            "name": "Device 2"
+        }
+    })
+    assert dc.num_devices() == 2
+
+    dev1 = dc.device("1234")
+    assert dev1 is not None
+    assert dev1.id == "1234"
+    assert dev1.name == "Device 1"
+    assert dev1.key is None
+
+    dev2 = dc.device("5678")
+    assert dev2 is not None
+    assert dev2.id == "5678"
+    assert dev2.name == "Device 2"
+    assert dev2.key is None
+
+    assert dc.device("ABCD") is None
+
+def test_devicesconfig_list():
+    dc = DevicesConfig([
+        {
+            "id": "1234",
+            "name": "Device 1"
+        }, {
+            "id": "5678",
+            "name": "Device 2"
+        }
+    ])
+    assert dc.num_devices() == 2
+
+    dev1 = dc.device("1234")
+    assert dev1 is not None
+    assert dev1.id == "1234"
+    assert dev1.name == "Device 1"
+    assert dev1.key is None
+
+    dev2 = dc.device("5678")
+    assert dev2 is not None
+    assert dev2.id == "5678"
+    assert dev2.name == "Device 2"
+    assert dev2.key is None
+
+    assert dc.device("ABCD") is None
